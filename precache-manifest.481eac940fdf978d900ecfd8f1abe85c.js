0b1e2027607f149056ca83688886cfc31f2c65fb
Nikky6666 <borbut.nika@mail.ru>
2020-02-03 17:14:36 +0300

    Updates

---
+++ precache-manifest.481eac940fdf978d900ecfd8f1abe85c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ef3a40a93dbefa2d007c68b9125a990",
+    "url": "/DnD_users_mentors_lists/index.html"
+  },
+  {
+    "revision": "d25bd0b35982e4a3ff3f",
+    "url": "/DnD_users_mentors_lists/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "397325218764d87e26ba",
+    "url": "/DnD_users_mentors_lists/static/js/2.958fd34b.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/DnD_users_mentors_lists/static/js/2.958fd34b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d25bd0b35982e4a3ff3f",
+    "url": "/DnD_users_mentors_lists/static/js/main.929162a1.chunk.js"
+  },
+  {
+    "revision": "0878959be2be49fe1a2b",
+    "url": "/DnD_users_mentors_lists/static/js/runtime-main.365f1058.js"
+  }
+]);
